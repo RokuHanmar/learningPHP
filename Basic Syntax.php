b7e2637666ab1b133032121318824e9c9469913b
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-07-31 16:16:24 +0100

    Create Basic Syntax.php

---
+++ Basic Syntax.php
@@ -0,0 +1,24 @@
+<?php
+
+    // Hello World
+    echo "Hello, World!";
+
+    // Variable declaration
+    $name = "Bob";
+    echo "<p>Hello, $name</p>";
+
+    // if statement
+    $age = 20;
+    if ($age >= 18) {
+        echo "<p>Adult</p>";
+    } else {
+        echo "<p>Child</p>";
+    }
+
+    // for loop
+    echo "<ul>";
+    for($i = 1; $i <= 5; $i++) {
+        echo "<li>$i</li>";
+    }
+    echo "</ul>;"
+?>
