6fb897f3733ae82382400e269ffd0adcbeecb719
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-10-10 19:58:31 +0100

    Update 04. Data Types.php
    Added a Number example

--- 04. Data Types.php
+++ 04. Data Types.php
@@ -1,5 +1,13 @@
 <?php
-    $a = "Test";
-    echo $a."<br>";
-    var_dump($a);
+
+    // String
+    $String = "Rohan";
+    echo $String."<br>";
+    var_dump($String);
+
+    // Number
+    $Number = 2310;
+    echo "<br>". $Number."<br>";
+    var_dump($Number);
+
 ?>
